df3b756ca6b9c2bb25e2f17ebc292f122fa1e839
OhLadyTam <tamara.radevic.92@gmail.com>
2017-01-30 23:47:09 +0100

    ponedeljak

---
+++ nst_proj/src/main/java/mainpack/nst_projApplication.java
@@ -0,0 +1,15 @@
+package mainpack;
+
+/**
+ * Created by DM on 1/27/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class nst_projApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(nst_projApplication.class, args);
+    }
+}
